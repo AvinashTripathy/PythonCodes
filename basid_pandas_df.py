9e5c6a41706393e7b015a02d54585bbff711fe9c
AvinashTripathy <36984515+AvinashTripathy@users.noreply.github.com>
2019-03-17 13:48:15 +0530

    Create basid_pandas_df.py

---
+++ basid_pandas_df.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+import xlsxwriter
+
+in_dir="/avinash/indir"
+
+file_in="Base_DS.csv"
+file_out="CM_Base_DS.csv"
+infile=pd.read_csv(os.path.join(in_dir,file_in),sep=',',dtype=object)
+outfile = infile[infile["size"] != "1"]
+outfile.to_csv(os.path.join(in_dir,file_out), sep=",",  header=True, index=False)
