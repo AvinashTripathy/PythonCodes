175667e1536f015870ba4bdea2f9e4a77e4ae41b
AvinashTripathy <36984515+AvinashTripathy@users.noreply.github.com>
2019-09-03 18:36:36 +0530

    Update basic_pandas_df.py

--- basic_pandas_df.py
+++ basic_pandas_df.py
@@ -5,7 +5,8 @@ import xlsxwriter
 in_dir="/avinash/indir"
 
 file_in="Base_DS.csv"
-file_out="CM_Base_DS.csv"
+file_out="Out_Base_DS.csv"
+
 infile=pd.read_csv(os.path.join(in_dir,file_in),sep=',',dtype=object)
 outfile = infile[infile["size"] != "1"]
 outfile.to_csv(os.path.join(in_dir,file_out), sep=",",  header=True, index=False)
